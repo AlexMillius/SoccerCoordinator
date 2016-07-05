8d11a46f821859bb3ed4ccd0d16a3412c1edb75d
Mohamed Lee <Mohamed@air-de-mohamed.domain_not_set.invalid>
2016-07-05 19:49:44 +0200

    - Correction of the implementation of each player individually. - Function that sort player according to the specification

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -3,28 +3,92 @@ import UIKit
 //MARK: Part1
 
 //Declaration of the empty array of player. Ideally a player should be a class with a name, height, experience and guardian constant. Those constant should be initialised with an init. But since I can't use a class implementation, my array of players is just an array of dictionaries String:NSObject.
-var players = [[Dictionary<String, NSObject>]]()
+var players = [[String:NSObject]]()
+
+//To avoid typo, I create four strings keys
+let name = "Name"
+let height = "Height"
+let experience = "Experience"
+let guardian = "Guardian"
 
 // implementation of each individual player. There is a high risk of typo with this method. A class would have been safer.
-let joeSmith = [["Name":"Joe Smith"],["Height":42],["Experience":true],["Guardian":"Jim and Jan Smith"]]
-let jillTanner = [["Name":"Jill Tanner"],["Height":36],["Experience":true],["Guardian":"Clara Tanner"]]
-let billBon = [["Name":"Bill Bon"],["Height":43],["Experience":true],["Guardian":"Sara and Jenny Bon"]]
-let evaGordon = [["Name":"Eva Gordon"],["Height":45],["Experience":false],["Guardian":"Wendy and Mike Gordon"]]
-let mattGill = [["Name":"Matt Gill"],["Height":40],["Experience":false],["Guardian":"Charles and Sylvia Gill"]]
-let kimmyStein = [["Name":"Kimmy Stein"],["Height":41],["Experience":false],["Guardian":"Bill and Hillary Stein"]]
-let sammyAdams = [["Name":"Sammy Adams"],["Height":45],["Experience":false],["Guardian":"Jeff Adams"]]
-let karlSaygan = [["Name":"Karl Saygan"],["Height":42],["Experience":true],["Guardian":"Heather Biedsoe"]]
-let suzaneGreenberg = [["Name":"Suzane Greenberg"],["Height":44],["Experience":true],["Guardian":"Henrietta Dumas"]]
-let salDali = [["Name":"Sal Dali"],["Height":41],["Experience":false],["Guardian":"Gala Dali"]]
-let joeKavalier = [["Name":"Joe Kavalier"],["Height":39],["Experience":false],["Guardian":"Sam and Elaine Kavalier"]]
-let benFinkeistein = [["Name":"Ben Finkeistein"],["Height":44],["Experience":false],["Guardian":"Aaron and Jill Finkeistein"]]
-let diegoSoto = [["Name":"Diego Soto"],["Height":41],["Experience":true],["Guardian":"Robin and Sarika Soto"]]
-let chloeAlaska = [["Name":"Chloe Alaska"],["Height":47],["Experience":false],["Guardian":"David and Jamie Alaska"]]
-let arnoldWillis = [["Name":"Arnold Willis"],["Height":43],["Experience":false],["Guardian":"Claire Willis"]]
-let philipHelm = [["Name":"Philip Helm"],["Height":44],["Experience":true],["Guardian":"Thomas Helm and Eva Jones"]]
-let lesClay = [["Name":"Les Clay"],["Height":42],["Experience":true],["Guardian":"Wynonna Brown"]]
-let herschelKrustofski = [["Name":"Herschel Krustofski"],["Height":45],["Experience":true],["Guardian":"Hyman and Rachel Krustofski"]]
+let joeSmith = [name:"Joe Smith",height:42,experience:true,guardian:"Jim and Jan Smith"]
+let jillTanner = [name:"Jill Tanner",height:36,experience:true,guardian:"Clara Tanner"]
+let billBon = [name:"Bill Bon",height:43,experience:true,guardian:"Sara and Jenny Bon"]
+let evaGordon = [name:"Eva Gordon",height:45,experience:false,guardian:"Wendy and Mike Gordon"]
+let mattGill = [name:"Matt Gill",height:40,experience:false,guardian:"Charles and Sylvia Gill"]
+let kimmyStein = [name:"Kimmy Stein",height:41,experience:false,guardian:"Bill and Hillary Stein"]
+let sammyAdams = [name:"Sammy Adams",height:45,experience:false,guardian:"Jeff Adams"]
+let karlSaygan = [name:"Karl Saygan",height:42,experience:true,guardian:"Heather Biedsoe"]
+let suzaneGreenberg = [name:"Suzane Greenberg",height:44,experience:true,guardian:"Henrietta Dumas"]
+let salDali = [name:"Sal Dali",height:41,experience:false,guardian:"Gala Dali"]
+let joeKavalier = [name:"Joe Kavalier",height:39,experience:false,guardian:"Sam and Elaine Kavalier"]
+let benFinkeistein = [name:"Ben Finkeistein",height:44,experience:false,guardian:"Aaron and Jill Finkeistein"]
+let diegoSoto = [name:"Diego Soto",height:41,experience:true,guardian:"Robin and Sarika Soto"]
+let chloeAlaska = [name:"Chloe Alaska",height:47,experience:false,guardian:"David and Jamie Alaska"]
+let arnoldWillis = [name:"Arnold Willis",height:43,experience:false,guardian:"Claire Willis"]
+let philipHelm = [name:"Philip Helm",height:44,experience:true,guardian:"Thomas Helm and Eva Jones"]
+let lesClay = [name:"Les Clay",height:42,experience:true,guardian:"Wynonna Brown"]
+let herschelKrustofski = [name:"Herschel Krustofski",height:45,experience:true,guardian:"Hyman and Rachel Krustofski"]
 
 //Implementation off all the 18 players into the players array
 players = [joeSmith,jillTanner,billBon,evaGordon,mattGill,kimmyStein,sammyAdams,karlSaygan,suzaneGreenberg,salDali,joeKavalier,benFinkeistein,diegoSoto,chloeAlaska,arnoldWillis,philipHelm,lesClay,herschelKrustofski]
 
+
+
+//MARK: Part 2
+
+// Declaration of the three teams
+var dragons = [[String:NSObject]]()
+var raptors = [[String:NSObject]]()
+var sharks = [[String:NSObject]]()
+
+func sortPlayerIntoThreeTeams(players players:[[String:NSObject]]){
+    
+    //Create three variables that keep trace of the number of experienced player in each team
+    var expDragons = 0
+    var expRaptors = 0
+    var expSharks = 0
+    
+    for player in players {
+        
+        //Check if the player has experience and asign it to a constant
+        let experience = player["Experience"] as! Bool
+        
+        if experience {
+            // The player is experimented
+            //Check the difference of experienced players in each teams and affect the experienced player in the team with the less experienced players. After the affectation in a team, the number of experienced players in the team is incremented.
+            if expDragons <= expRaptors && expDragons <= expSharks {
+                dragons.append(player)
+                expDragons += 1
+            }else if expRaptors <= expDragons && expRaptors <= expSharks {
+                raptors.append(player)
+                expRaptors += 1
+            } else if expSharks <= expDragons && expSharks <= expRaptors {
+                sharks.append(player)
+                expSharks += 1
+            }
+        } else {
+            // The player is not experimented
+            // Affect the inexperimented player in the team with the less amount of players.
+            if dragons.count <= raptors.count && dragons.count <= sharks.count {
+                dragons.append(player)
+            }else if raptors.count <= dragons.count && raptors.count <= sharks.count {
+                raptors.append(player)
+            } else if expSharks <= dragons.count && sharks.count <= raptors.count {
+                sharks.append(player)
+            }
+        }
+    }
+}
+
+sortPlayerIntoThreeTeams(players: players)
+
+print(dragons)
+print(dragons.count)
+print("\n")
+print(raptors)
+print(raptors.count)
+print("\n")
+print(sharks)
+print(sharks.count)
