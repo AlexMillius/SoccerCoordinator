c4075937c0c1e0f7c0528ad4890fd31abb0bc3ed
Mohamed Lee <Mohamed@air-de-mohamed.domain_not_set.invalid>
2016-07-07 12:28:35 +0200

    Part 1 ok Part 2 ok Part 3 ok Extra credit ok Il manque des commentaires. Le programme doit être modifié pour accepter plus de 3 team

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -38,61 +38,124 @@ let herschelKrustofski = [name:"Herschel Krustofski",height:45,experience:true,g
 players = [joeSmith,jillTanner,billBon,evaGordon,mattGill,kimmyStein,sammyAdams,karlSaygan,suzaneGreenberg,salDali,joeKavalier,benFinkeistein,diegoSoto,chloeAlaska,arnoldWillis,philipHelm,lesClay,herschelKrustofski]
 
 
-
-//MARK: Part 2
+//MARK: Part 2 (With extra credit)
 
 // Declaration of the three teams
 var dragons = [[String:NSObject]]()
 var raptors = [[String:NSObject]]()
 var sharks = [[String:NSObject]]()
 
-
-func sortPlayerIntoThreeTeams(players players:[[String:NSObject]]){
+//There is 3 phases to this function
+// Step 1. separate experimented player from non experimented
+// Step 2. order player from smallest to tallest
+// Step 3. affect the player evenly into three teams
+func sortPlayerIntoThreeTeams(players players:[[String:NSObject]]) -> (team1:[[String:NSObject]], team2:[[String:NSObject]], team3:[[String:NSObject]]){
     
-    //Create three variables that keep trace of the number of experienced player in each team
-    var expDragons = 0
-    var expRaptors = 0
-    var expSharks = 0
+    //STEP 1.
+    //Create an Array of experimented players and one of inexperimented players
+    var playersXp = [[String:NSObject]]()
+    var playersNonXp = [[String:NSObject]]()
     
     for player in players {
-        
         //Check if the player has experience and asign it to a constant
         let experience = player["Experience"] as! Bool
         
         if experience {
-            // The player is experimented
-            // 1. Check the difference of experienced players in each teams
-            // 2. Affect the experienced player in the team with the less experienced players
-            // 3. Increment the number of experienced player in teams.
-            if expDragons <= expRaptors && expDragons <= expSharks {
-                dragons.append(player)
-                expDragons += 1
-            }else if expRaptors <= expDragons && expRaptors <= expSharks {
-                raptors.append(player)
-                expRaptors += 1
-            } else if expSharks <= expDragons && expSharks <= expRaptors {
-                sharks.append(player)
-                expSharks += 1
-            }
+            // The player is experimented, affect it to the experimented array
+            playersXp.append(player)
         } else {
-            // The player is not experimented
-            // Affect the inexperimented player in the team with the less amount of players.
-            if dragons.count <= raptors.count && dragons.count <= sharks.count {
-                dragons.append(player)
-            }else if raptors.count <= dragons.count && raptors.count <= sharks.count {
-                raptors.append(player)
-            } else if expSharks <= dragons.count && sharks.count <= raptors.count {
-                sharks.append(player)
-            }
+            // The player is Not experimented, affect it to the Non experimented array
+            playersNonXp.append(player)
         }
     }
+    
+    //STEP 2.
+    //order the player from smallest to tallest
+    func sortPlayers(players: [[String:NSObject]]) -> [[String:NSObject]]{
+        
+        // create an internal players array for manipulation
+        var players = players
+        
+        // create a variable to keep track of the number of players in order
+        var playersInOrder = 0
+        
+        //While all the players aren't in order, the players are sorted
+        repeat {
+            // (re)initialize the number of players in order
+            playersInOrder = 0
+            
+            // Loop through all players
+            for index in 0..<players.count {
+                //extract the currenct player and his height
+                let player = players[index]
+                let height = player["Height"] as! Int
+                
+                //Since we swap with the next player, we make sure that we aren't at the end of the array
+                if index < players.count - 1 {
+                    //extract the next player and his height
+                    let nextPlayer = players[index + 1]
+                    let nextHeight = nextPlayer["Height"] as! Int
+                    
+                    //check if the current player is taller than the next
+                    if height > nextHeight {
+                        // The player is taller, we swap the two players
+                        players[index + 1] = player
+                        players[index] = nextPlayer
+                    } else {
+                        //The player and the next player are in good order or the same height
+                        playersInOrder += 1
+                    }
+                }
+            }
+        } while playersInOrder < players.count - 1
+        
+        return players
+    }
+    
+    playersXp = sortPlayers(playersXp)
+    playersNonXp = sortPlayers(playersNonXp)
+    
+    //STEP 3.
+    var team1 = [[String:NSObject]]()
+    var team2 = [[String:NSObject]]()
+    var team3 = [[String:NSObject]]()
+    
+    func affectPlayers(players: [[String:NSObject]]){
+        var players = players
+        var order = 1
+        //affect players one by one into the teams while there is still players to affect.
+        repeat {
+            // if we affect the player in the same order every time, the second team will have a less average height, and the third even less. To avoid that, the order of affectation is changed every time.
+            if order == 1 {
+                team1.append(players.removeLast())
+                team2.append(players.removeLast())
+                team3.append(players.removeLast())
+                order = 2
+            } else if order == 2 {
+                team2.append(players.removeLast())
+                team3.append(players.removeLast())
+                team1.append(players.removeLast())
+                order = 3
+            } else {
+                team3.append(players.removeLast())
+                team2.append(players.removeLast())
+                team1.append(players.removeLast())
+                order = 1
+            }
+        } while players.isEmpty == false
+    }
+    
+    affectPlayers(playersXp)
+    affectPlayers(playersNonXp)
+    
+    return (team1, team2, team3)
 }
 
-sortPlayerIntoThreeTeams(players: players)
 
-dragons
-raptors
-sharks
+
+dragons = sortPlayerIntoThreeTeams(players: players).team1
+raptors = sortPlayerIntoThreeTeams(players: players).team2
+sharks = sortPlayerIntoThreeTeams(players: players).team3
 
 
 //MARK: Part 3
@@ -148,3 +211,58 @@ func sendLetterToGuardian(league league:[String:[[String:NSObject]]]) -> [String
 }
 
 let letters = sendLetterToGuardian(league: league)
+
+
+//MARK: verification
+func checkTheAverageHeight(team1:[[String:NSObject]], team2:[[String:NSObject]], team3:[[String:NSObject]]) -> Double {
+    
+    func getAverageHeightOfTheTeam(team:[[String:NSObject]]) -> Double{
+        var totalHeight = Double()
+        for player in team {
+            let height = player["Height"] as! Double
+            totalHeight += height
+        }
+        return totalHeight / Double(team.count)
+    }
+    
+    let avgTeam1 = getAverageHeightOfTheTeam(team1)
+    let avgTeam2 = getAverageHeightOfTheTeam(team2)
+    let avgTeam3 = getAverageHeightOfTheTeam(team3)
+    
+    func sortTeamAvg(teams:[Double]) -> [Double]{
+        var teamsAvgInOrder = 0
+        var teams = teams
+        repeat {
+            teamsAvgInOrder = 0
+            for index in 0..<teams.count {
+                if index < teams.count - 1 {
+                    if teams[index] > teams[index + 1] {
+                        // The player is taller, we swap the two players
+                        let tempTeam = teams[index + 1]
+                        teams[index + 1] = teams[index]
+                        teams[index] = tempTeam
+                    } else {
+                        //The player and the next player are in good order or the same height
+                        teamsAvgInOrder += 1
+                    }
+                }
+            }
+        } while teamsAvgInOrder < teams.count - 1
+        return teams
+    }
+    
+    let sortedAvgTeam = sortTeamAvg([avgTeam1,avgTeam2,avgTeam3])
+    
+    return sortedAvgTeam[2] - sortedAvgTeam[0]
+}
+
+checkTheAverageHeight(raptors, team2: sharks, team3: dragons)
+
+
+
+
+
+
+
+
+
